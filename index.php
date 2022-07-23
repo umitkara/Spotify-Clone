3aa5ae4bb2b088d3e55b4bf0ce861bbcbf1436e9
umitkara <sakarian54@gmail.com>
2022-07-23 23:45:55 +0300

    Now playing bar implemented.

--- index.php
+++ index.php
@@ -21,8 +21,39 @@ else {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Spotify</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/fontawesome/css/all.css">
 </head>
 <body>
-    
+    <div id="nowPlaying" class="nowPlayingContainer">
+        <div id="nowPlayingBar" class="nowPlayingBar">
+            <div class="nowPlayingLeft"></div>
+            <div class="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle" title="Shuffle">
+                            <i class="fa-regular fa-shuffle"></i>
+                        </button>
+                        <button class="controlButton previous" title="Previous">
+                            <i class="fa-regular fa-step-backward"></i>
+                        </button>
+                        <button class="controlButton play" title="Play">
+                            <i class="fa-regular fa-circle-play"></i>
+                        </button>
+                        <button class="controlButton pause" title="Pause" style="display: none;">
+                            <i class="fa-regular fa-pause-circle"></i>
+                        </button>
+                        <button class="controlButton next" title="Next">
+                            <i class="fa-regular fa-step-forward"></i>
+                        </button>
+                        <button class="controlButton repeat" title="Repeat">
+                            <i class="fa-regular fa-repeat"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="nowPlayingRight"></div>
+        </div>
+    </div>
 </body>
 </html>
