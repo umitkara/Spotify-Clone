66e142891ba29ce132dcfbd651c116e48af5ee8a
umitkara <sakarian54@gmail.com>
2022-07-24 16:15:14 +0300

    Main container implemented. Index page seperated into header and footer.

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+                </div>
+            </div>
+        </div>
+        <div id="nowPlaying" class="nowPlayingContainer">
+            <?php include("includes/now_playing_bar.php") ?>
+        </div>
+    </div>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<?php
+
+include("includes/dbconfig.php");
+include("includes/config.php");
+
+
+if(isset($_SESSION['user_id']))
+{
+    $user_id = $_SESSION['user_id'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spotify Clone</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/fontawesome/css/all.css">
+    <link rel="icon" href="assets/images/favicon.svg" type="image/svg+xml">
+</head>
+<body>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <?php include("includes/navbar_container.php") ?>
+            <div class="mainViewContainer">
+                <div class="mainContent">
