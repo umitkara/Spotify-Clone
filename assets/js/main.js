1a663517b1ea9293df91fd82831b1b442e69dd48
umitkara <sakarian54@gmail.com>
2022-07-25 00:26:13 +0300

    Audio element class implemented.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
 function openPage(page_url) {
     window.location.href = page_url;
 }
+
+let current_playlist = [];
+var audio_element;
+
+class AudioElm {
+
+    constructor(src) {
+        this.current_playing = null;
+        this.src = src;
+        this.audio = document.createElement('audio');
+    }
+
+    setTrack(track) {
+        this.audio.src = track;
+    }
+
+    play() {
+        this.audio.play();
+    }
+
+    pause() {
+        this.audio.pause();
+    }
+}
