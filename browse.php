051c1eb9fa006a9bfd10595f40bb9426bf40668a
umitkara <sakarian54@gmail.com>
2022-07-25 23:14:27 +0300

    Browse page implemented.

---
+++ browse.php
@@ -0,0 +1,21 @@
+<?php include("includes/include_files.php"); ?>
+<div class="pageHeader">
+    You might be interested in...
+</div>
+<div class="gridViewContainer">
+    <?php
+    $album_query = mysqli_query($connection, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+    while ($row = mysqli_fetch_array($album_query)) {
+        echo "<div class='gridViewItem'>
+                        <span role='link' tabindex='0' onclick='open_page(\"album.php?id=" . $row['id'] . "\")'>
+                            <img src='" . $row['albumArt'] . "'>
+                            <div class='gridViewInfo'>"
+            . $row['title'] .
+            "</div>
+                        </span>
+                    </div>";
+    }
+
+    ?>
+</div>
