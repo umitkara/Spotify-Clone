dff11cd98ad1047f6932372e961f2b0bd8567f26
umitkara <sakarian54@gmail.com>
2022-07-25 21:00:44 +0300

    Anchor links changed to onclick.

--- includes/navbar_container.php
+++ includes/navbar_container.php
@@ -6,7 +6,7 @@
         </a>
         <div class="group">
             <div class=" navItem">
-                <a href="search.php">
+                <a onclick="open_page('search.php')">
                     <i class="fa-regular fa-search"></i>
                     Search
                 </a>
@@ -14,19 +14,19 @@
         </div>
         <div class="group">
             <div class=" navItem">
-                <a href="browse.php">
+                <a onclick="open_page('browse.php')">
                     <i class="fa-regular fa-grid"></i>
                     Browse
                 </a>
             </div>
             <div class=" navItem">
-                <a href="your_music.php">
+                <a onclick="open_page('your_music.php')">
                     <i class="fa-regular fa-compact-disc"></i>
                     Your Music
                 </a>
             </div>
             <div class=" navItem">
-                <a href="profile.php">
+                <a onclick="open_page('profile.php')">
                     <i class="fa-regular fa-user"></i>
                     Profile
                 </a>
