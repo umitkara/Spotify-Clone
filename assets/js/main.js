4bf07432959c352b3b6ece7877eab02c4ffe5fc3
umitkara <sakarian54@gmail.com>
2022-07-25 10:47:44 +0300

    Canplay event listener added to update song length. Timeupdate event listener added.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,26 @@ function openPage(page_url) {
 let current_playlist = [];
 var audio_element;
 
+function time_format(secs) {
+    // return mm:ss format
+    return Math.floor(secs / 60) + ":" + (secs % 60 < 10 ? "0" : "") + Math.floor(secs % 60);
+}
+
 class AudioElm {
 
     constructor(src) {
         this.current_playing = null;
         this.src = src;
         this.audio = document.createElement('audio');
+
+        this.audio.addEventListener('canplay', () => {
+            $('.progressTime.remaining').text(time_format(this.audio.duration));
+        });
+        
+        this.audio.addEventListener('timeupdate', () => {
+            $('.progressTime.current').text(time_format(this.audio.currentTime));
+            $('.progressBar .progress').css('width', (this.audio.currentTime / this.audio.duration) * 100 + '%');
+        });
     }
 
     setTrack(track) {
