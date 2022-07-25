5f4ed1b4dfc98c2604a70c57acdca1c107f61c68
umitkara <sakarian54@gmail.com>
2022-07-25 10:18:53 +0300

    Ajax request handler for song implemented.

--- includes/handlers/ajax/get_song_json.php
+++ includes/handlers/ajax/get_song_json.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "asd"
+include("../../../includes/dbconfig.php");
+include("../../../includes/config.php");
+
+if(isset($_POST['song_id'])) {
+    $song_id = $_POST['song_id'];
+    $query = mysqli_query($connection, "SELECT * FROM songs WHERE id='$song_id'");
+    $result_array = mysqli_fetch_array($query);
+    echo json_encode($result_array);
+}
 
 ?>
