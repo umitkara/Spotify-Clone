f5c93b9083c2b22524612c9dfa4bbd993f0a0e45
umitkara <sakarian54@gmail.com>
2022-07-26 20:05:56 +0300

    Playlist class updated. Song count and songs getters implemented.

--- includes/classes/Playlist.php
+++ includes/classes/Playlist.php
@@ -5,12 +5,14 @@ class Playlist {
     private $name;
     private $owner;
 
-    public function __construct($connection, $data)
+    public function __construct($connection, $id)
     {
         $this->connection = $connection;
-        $this->id = $data['id'];
-        $this->name = $data['name'];
-        $this->owner = $data['owner'];
+        $this->id = $id;
+        $query = mysqli_query($connection, "SELECT * FROM playlists WHERE id = '$id'");
+        $row = mysqli_fetch_array($query);
+        $this->name = $row['name'];
+        $this->owner = $row['owner'];
     }
 
     public function get_id()
@@ -30,18 +32,24 @@ class Playlist {
 
     public function get_owner()
     {
-        $u = new User($this->connection, $this->owner);
+        $u = new User($this->connection, (int)$this->owner);
         return $u->get_username();
     }
 
     public function get_songs()
     {
-        $query = mysqli_query($this->connection, "SELECT * FROM playlist_songs WHERE playlist_id = $this->id");
+        $query = mysqli_query($this->connection, "SELECT * FROM playlistsongs WHERE playlistID = $this->id");
         $songs = array();
         while($row = mysqli_fetch_assoc($query)) {
-            array_push($songs, $row['song_id']);
+            array_push($songs, $row['songID']);
         }
         return $songs;
+    }
+
+    public function song_count()
+    {
+        $query = mysqli_query($this->connection, "SELECT * FROM playlistsongs WHERE playlistID = $this->id");
+        return mysqli_num_rows($query);
     }
 }
 ?>
