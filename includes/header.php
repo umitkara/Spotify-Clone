b1abd68db6fc672fd7db9a48936612c21b26e375
umitkara <sakarian54@gmail.com>
2022-07-26 18:01:42 +0300

    Playlist and user classes added as include.

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,8 @@ include("includes/config.php");
 include("includes/classes/Artist.php");
 include("includes/classes/Album.php");
 include("includes/classes/Song.php");
+include("includes/classes/User.php");
+include("includes/classes/Playlist.php");
 
 if(isset($_SESSION['user_id']))
 {
--- includes/include_files.php
+++ includes/include_files.php
@@ -6,6 +6,8 @@ if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
     include("includes/classes/Artist.php");
     include("includes/classes/Album.php");
     include("includes/classes/Song.php");
+    include("includes/classes/User.php");
+    include("includes/classes/Playlist.php");
 } else {
     include("includes/header.php");
     include("includes/footer.php");
