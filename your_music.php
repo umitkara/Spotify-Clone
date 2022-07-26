0ae5713a0eb0eeb52ae713cb7540163b644536a2
umitkara <sakarian54@gmail.com>
2022-07-26 18:15:05 +0300

    Your music page playlist gridview implemented.

--- your_music.php
+++ your_music.php
@@ -14,8 +14,8 @@ include("includes/include_files.php");
         </div>
         <?php
             $username = $_SESSION['user_id'];
-            $user_query = mysqli_query($connection, "SELECT id FROM users WHERE username = '$username'");
-            $user_id = mysqli_fetch_assoc($user_query)['id'];
+            $user = new User($connection, $username);
+            $user_id = $user->get_id();
             $playlist_query = mysqli_query($connection, "SELECT * FROM playlists WHERE owner = '$user_id'");
             if(mysqli_num_rows($playlist_query) == 0) {
                 echo "<span>No playlists found</span>";
@@ -25,19 +25,19 @@ include("includes/include_files.php");
                 $playlist_name = $row['name'];
                 echo "<div class='gridViewItem'>
                         <div class='playlistItem'>
-                            <div class='playlistThumb'>
+                            <div class='playlistThumb' role='link' tab-index='0' onclick='open_page(\"playlist.php?id=$playlist_id\")'>
                                 <img src='assets\images\album_default.jpg' draggable='false'>
                             </div>
-                            <div class='playlistName'>
+                            <div class='playlistName' role='link' tab-index='0' onclick='open_page(\"playlist.php?id=$playlist_id\")'>
                                 $playlist_name
                             </div>
                             <div class='playlistItemControls'>
                                 <input type='hidden' class='playlistId' value='$playlist_id'>
-                                <button class='button buttonSmall' onclick='showPlaylistSongs($playlist_id)'>
+                                <button class='button buttonSmall green' onclick='showPlaylistSongs($playlist_id)'>
                                     <i class='fa-regular fa-chevron-right'></i>
                                 </button>
-                                <button class='button buttonSmall' onclick='showPlaylistOptions($playlist_id)'>
-                                    <i class='fa-regular fa-chevron-right'></i>
+                                <button class='button buttonSmall red' onclick='showPlaylistOptions($playlist_id)'>
+                                    <i class='fa-regular fa-xmark'></i>
                                 </button>
                             </div>
                         </div>
