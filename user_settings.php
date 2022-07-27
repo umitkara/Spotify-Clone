84ef43131f02518c9eb06f99cd1e25c75c7d87f7
umitkara <sakarian54@gmail.com>
2022-07-27 20:38:25 +0300

    Update name field added.

--- user_settings.php
+++ user_settings.php
@@ -5,6 +5,19 @@ $user = new User($connection, $_SESSION['user_id']);
 
 <div class="userDetails">
     <div class="detailContainer">
+        <h2>Name</h2>
+        <div class="nameDetails">
+            <label for="firstName">First Name</label>
+            <input id="firstName" type="text" name="firstName" value="<?php echo $user->get_first_name(); ?>">
+            <label for="lastName">Last Name</label>
+            <input id="lastName" type="text" name="lastName" value="<?php echo $user->get_last_name(); ?>">
+        </div>
+        <span id="nameMessage" class="message"></span>
+        <button class="button green" onclick="update_name()">
+            Update
+        </button>
+    </div>
+    <div class="detailContainer">
         <h2>E-mail</h2>
         <input id="email" type="text" class="email" name="email" value="<?php echo lcfirst($user->get_email()); ?>">
         <span id="emailMessage" class="message"></span>
